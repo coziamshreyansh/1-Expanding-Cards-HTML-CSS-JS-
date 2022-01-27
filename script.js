cc5a10f7722d94aa1ad912ef4b1fbe9d0c8625cf
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2022-01-27 23:54:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// query selector for selecting panels
+const panels = document.querySelectorAll('.panel') 
+
+panels.forEach((panels)=> {
+  panels.addEventListener('click', () => {
+    removeActiveClasses()
+    panels.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panels => {
+    panels.classList.remove('active')
+  })
+}
